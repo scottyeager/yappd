b1490a7a154791e72287b3f7081a9a0f4167ecc8
Scott Yeager <yeagersm@gmail.com>
2024-08-16 10:00:50 -0700

    Add save function

--- pdict.py
+++ pdict.py
@@ -20,8 +20,7 @@ class PDict:
         return self.dict[item]
     
     def __setitem__(self, item, value):
-        self.con.execute("INSERT OR REPLACE INTO data VALUES(?, ?, ?)", (self.dictname, jsonpickle.dumps(item), jsonpickle.dumps(value)))
-        self.con.commit()
+        self._write(item, value)
         self.dict[item] = value
 
     def __delitem__(self, item):
@@ -32,7 +31,23 @@ class PDict:
     def __repr__(self):
         return self.dict.__repr__()
     
+    def _write(self, key, value):
+        self.con.execute("INSERT OR REPLACE INTO data VALUES(?, ?, ?)", (self.dictname, jsonpickle.dumps(key), jsonpickle.dumps(value)))
+        self.con.commit()
+    
     def update(self, input_dict):
         for key, value in input_dict.items():
             if key not in self.dict or self.dict[key] != value:
                 self[key] = value
+
+    def save(self, *args):
+        """Updates the state stored on disk to match the in memory dict. This is only intended to be used to synchronize any mutations of stored objects. It's not intended that users modify the internal dict directly.
+
+        If args are given, they are the keys to be saved. With no args, save all keys.
+        """
+        if len(args) > 0:
+            for arg in args:
+                self._write(arg, self.dict[arg])
+        else:
+            for key, value in self.dict.items():
+                self._write(key, value)

d8854bcb0123c8a9f8fdba7616f7a211d14e9bfe
Scott Yeager <yeagersm@gmail.com>
2024-08-16 10:22:22 -0700

    Renaming

--- pdict.py
+++ pdict.py
@@ -2,42 +2,42 @@ import sqlite3
 import jsonpickle
 
 class PDict:
-    def __init__(self, dbfile, dictname="default"):
+    def __init__(self, dbfile, name="default"):
         self.dbfile = dbfile
-        self.dictname = dictname
+        self.name = name
 
-        self.dict = {}
+        self._dict = {}
         self.con = sqlite3.connect(dbfile)
         self.con.execute("PRAGMA journal_mode=wal")
         self.con.execute("CREATE TABLE IF NOT EXISTS data(dict, key, value)")
         self.con.execute("CREATE UNIQUE INDEX IF NOT EXISTS dict_key ON data (dict, key)")
 
-        results = self.con.execute("SELECT key, value FROM data WHERE dict=?", (dictname,)).fetchall()
+        results = self.con.execute("SELECT key, value FROM data WHERE dict=?", (name,)).fetchall()
         for key, value in results:
-            self.dict[jsonpickle.loads(key)] = jsonpickle.loads(value)            
+            self._dict[jsonpickle.loads(key)] = jsonpickle.loads(value)            
 
-    def __getitem__(self, item):
-        return self.dict[item]
+    def __getitem__(self, key):
+        return self._dict[key]
     
-    def __setitem__(self, item, value):
-        self._write(item, value)
-        self.dict[item] = value
+    def __setitem__(self, key, value):
+        self._write(key, value)
+        self._dict[key] = value
 
-    def __delitem__(self, item):
-        self.con.execute("DELETE FROM data WHERE dict=? AND key=?", (self.dictname, jsonpickle.dumps(item)))
+    def __delitem__(self, key):
+        self.con.execute("DELETE FROM data WHERE dict=? AND key=?", (self.name, jsonpickle.dumps(key)))
         self.con.commit()
-        del self.dict[item]
+        del self._dict[key]
 
     def __repr__(self):
-        return self.dict.__repr__()
+        return self._dict.__repr__()
     
     def _write(self, key, value):
-        self.con.execute("INSERT OR REPLACE INTO data VALUES(?, ?, ?)", (self.dictname, jsonpickle.dumps(key), jsonpickle.dumps(value)))
+        self.con.execute("INSERT OR REPLACE INTO data VALUES(?, ?, ?)", (self.name, jsonpickle.dumps(key), jsonpickle.dumps(value)))
         self.con.commit()
     
     def update(self, input_dict):
         for key, value in input_dict.items():
-            if key not in self.dict or self.dict[key] != value:
+            if key not in self._dict or self._dict[key] != value:
                 self[key] = value
 
     def save(self, *args):
@@ -47,7 +47,7 @@ class PDict:
         """
         if len(args) > 0:
             for arg in args:
-                self._write(arg, self.dict[arg])
+                self._write(arg, self._dict[arg])
         else:
-            for key, value in self.dict.items():
+            for key, value in self._dict.items():
                 self._write(key, value)
